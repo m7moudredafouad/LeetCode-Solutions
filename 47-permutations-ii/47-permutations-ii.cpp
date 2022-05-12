7314b12efa0cf2d261f92746cbae410c24d0e110
Mahmoud Reda <47112939+m7moudredafouad@users.noreply.github.com>
2022-05-12 03:29:30 +0200

    Time: 7 ms (80.95%), Space: 8.6 MB (81.01%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,15 +1,5 @@
 class Solution {
   private: 
-  vector<int> usedIndex;
-  
-  bool checkIsUsedIndex(int x) {
-    for(int i = 0; i < usedIndex.size(); i++) {
-      if(usedIndex[i] == x) return true;
-    }
-    
-    return false;
-  }
-    
   void findPermutation(vector<pair<int, int>>& nums, vector<vector<int>> &addHere, vector<int>& currPerm, int remainingLength) {
     if(remainingLength == 0) {
       addHere.push_back(currPerm);
@@ -18,13 +8,11 @@ class Solution {
     
     for(int i = 0; i < nums.size(); i++) {
       if(nums[i].second > 0) {
-        // usedIndex.push_back(i);
         nums[i].second--;
         currPerm.push_back(nums[i].first);
         findPermutation(nums, addHere, currPerm, remainingLength - 1);
         currPerm.pop_back();
         nums[i].second++;
-        // usedIndex.pop_back();
       }
     }
   }
