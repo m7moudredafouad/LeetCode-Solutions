8e4880f76a4e33437e0474be216dc3933edd0508
Mahmoud Reda <47112939+m7moudredafouad@users.noreply.github.com>
2022-05-22 03:30:49 +0200

    Time: 299 ms (11.00%), Space: 20.2 MB (12.05%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,37 +1,25 @@
 class Solution {
     
-    bool countPalindromicBoth(string &s, vector<vector<int>>& saved, int start, int end) {
+    bool checkPalindromic(string &s, int start, int end) {
         if(start >= s.length()) return false;
         
         if(start >= end) return true;
         
         if(s[start] != s[end]) return false;
         
-        return countPalindromicBoth(s, saved, start+1, end - 1);
+        return checkPalindromic(s, start+1, end - 1);
     }
     
     int countPalindromic(string &s, vector<vector<int>>& saved, int start, int end) {
-        if(start >= s.length() || end < 0 || end <= start) return 0;
+        if(start >= s.length() ||
+           end <= start ||
+           saved[start][end] != -1) return 0;        
         
-        // cout << start << ": " << end << endl;
+        saved[start][end] = countPalindromic(s, saved, start + 1, end) +
+                            countPalindromic(s, saved, start, end - 1);
         
-        if(saved[start][end] != -1) return 0;
+        if(s[start] == s[end]) saved[start][end] += checkPalindromic(s, start + 1, end - 1);
         
-        int areEqual = 0;
-        if(s[start] == s[end]) {
-            areEqual = 1;
-        }
-        
-        int shiftLeft = countPalindromic(s, saved, start + 1, end);
-        
-        int shiftRight = countPalindromic(s, saved, start, end - 1);
-        
-        if(areEqual) {
-            areEqual = countPalindromicBoth(s, saved, start + 1, end - 1);
-        }
-
-        
-        saved[start][end] = shiftLeft + shiftRight + areEqual;
         return saved[start][end];
     }
 public:
