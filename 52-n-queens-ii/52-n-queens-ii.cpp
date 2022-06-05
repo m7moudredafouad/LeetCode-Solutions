e1dfd9820d72b41f7c20aa8814c3ac7f15ca00dc
Mahmoud Reda <47112939+m7moudredafouad@users.noreply.github.com>
2022-06-05 13:33:40 +0200

    Time: 9 ms (47.27%), Space: 6.6 MB (38.33%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+        bool isValid( vector<string> &tmp,int x, int y, int n) {
+        for(int i = 0; i < n; i++) {
+            if(tmp[i][y] == 'Q') return false;
+        }
+
+        for(int i = x, j = y; i >= 0 && j < n; i--, j++) {
+            if(tmp[i][j] == 'Q') return false;
+        }
+
+        for(int i = x, j = y; i >= 0 && j >= 0; i--, j--) {
+            if(tmp[i][j] == 'Q') return false;
+        }
+        
+        return true;
+    }
+    
+    void solveQueen(vector<vector<string>> &sol, vector<string> &tmp,int index, int n) {
+        if(index == n) {
+            sol.push_back(tmp);
+            return;
+        }
+        for(int i = 0; i < n; i++) {
+            if(isValid(tmp, index, i, n)) {
+                tmp[index][i] = 'Q';
+                solveQueen(sol, tmp, index+1, n);
+                tmp[index][i] = '.';            
+            }
+        }
+    }
+public:
+    int totalNQueens(int n) {
+        vector<vector<string>> sol;
+        vector<string> tmp(n, string(n, '.'));
+        solveQueen(sol, tmp, 0, n);
+        return sol.size();
+    }
+};
