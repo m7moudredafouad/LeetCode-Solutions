61ae6e7d11efd1a4f54b09813d11a024f70ee41b
Mahmoud Reda <47112939+m7moudredafouad@users.noreply.github.com>
2022-06-09 02:15:12 +0200

    Time: 19 ms (51.56%), Space: 15.7 MB (6.15%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,28 +1,20 @@
 class Solution {
-    int binarySearch(vector<int>& numbers, int start, int end, int target) {
-        if(start > end) return -1;
-        int mid = (start + end) / 2;
-        
-        if(numbers[mid] > target) {
-            return binarySearch(numbers, start, mid-1, target);
-        } else if(numbers[mid] < target) {
-            return binarySearch(numbers, mid+1, end, target);
-        } else {
-            return mid;
-        }
-    }
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
+        int left = 0, right = numbers.size() - 1;
         vector<int> sol(2);
-        int binaryResult = -1;
-        for(int i = 0; i < numbers.size(); ++i) {
-            binaryResult = binarySearch(numbers, i+1, numbers.size()-1, target - numbers[i]);
-            if(binaryResult > -1) {
-                sol[0] = i+1;
-                sol[1] = binaryResult+1;
+        
+        while(left < right) {
+            if(numbers[left] + numbers[right] > target) {
+                right--;
+            } else if(numbers[left] + numbers[right] < target) {
+                left++;
+            } else {
+                sol[0] = left+1;
+                sol[1] = right+1;
+                break;
             }
         }
-        
         return sol;
     }
 };
