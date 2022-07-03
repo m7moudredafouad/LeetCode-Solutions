99a5a913dbe8ddc5d528cf357316c2e786a1e42c
Mahmoud Reda <47112939+m7moudredafouad@users.noreply.github.com>
2022-07-03 11:53:01 +0200

    Time: 6 ms (43.60%), Space: 7 MB (75.45%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
         signed char sign1 = 1, sign2 = -1;
-        int secondLastAdded = 0;
         int size1 = 1, size2 = 1;
         
         for(int i = 1; i < nums.size(); i++) {
